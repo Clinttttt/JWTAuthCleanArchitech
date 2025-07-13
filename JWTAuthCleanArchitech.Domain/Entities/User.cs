b2205426c2c6e8e5624c4cface181fbdedbb3a12
Clinttttt <clintvillanueva82@gmail.com>
2025-07-13 13:07:03 +0800

    UpdatedDomain

--- JWTAuthCleanArchitech.Domain/Entities/User.cs
+++ JWTAuthCleanArchitech.Domain/Entities/User.cs
@@ -14,7 +14,7 @@ namespace JWTAuthCleanArchitech.Domain.Entities
         public string Role { get; set; } = string.Empty;
         public string? RefreshToken { get; set; }
         public DateTime RefreshTokenExpiryTime { get; set; }
-        public string UserEmail { get; set; } = string.Empty; 
+     
 
     }
 }
---
+++ JWTAuthCleanArchitech.Domain/Models/RefreshTokenDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWTAuthCleanArchitech.Domain.Models
+{
+    public class RefreshTokenDto
+    {
+        public Guid UserId { get; set; }
+        public required string RefreshToken { get; set; }
+    }
+}
